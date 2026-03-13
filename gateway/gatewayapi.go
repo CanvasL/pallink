ed4d4ffb0cab7cf53514ba5dd06911e631ddafb2
Fan <746591811@qq.com>
2026-03-13 11:15:09 +0800

    fix: CORS, swagger auth

--- gateway/gatewayapi.go
+++ gateway/gatewayapi.go
@@ -23,7 +23,11 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
+	server := rest.MustNewServer(
+		c.RestConf,
+		rest.WithCors(),
+		rest.WithCorsHeaders("Authorization", "Content-Type", "X-Requested-With"),
+	)
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
